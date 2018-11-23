1b6e3c97f6604b470152fa98f18b54a330a55d1a
Samuel Hornsey <hornseysam498@gmail.com>
2018-11-23 11:31:42 +1100

    init commit

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import { h, Component } from 'preact';
+
+export default class App extends Component {
+	render() {
+		return (
+			<div class="app">
+        <h2>Yay preact!</h2>
+			</div>
+		);
+	}
+}
